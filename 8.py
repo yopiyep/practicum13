acb8c5a54d05b200018d2cba05ee8d1f49c17428
yopiyep <artemkol200831@gmail.com>
2026-05-16 14:51:31 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -2,7 +2,11 @@ from itertools import combinations
 
 
 elements = sorted(set(input().split()))
-subsets = [list(comb) for r in range(len(elements) + 1) for comb in combinations(elements, r)]
+subsets = []
+
+for r in range(len(elements) + 1):           # внешний цикл по размерам
+    for comb in combinations(elements, r):   # внутренний цикл по комбинациям
+        subsets.append(list(comb))
 
 for subset in subsets:
     print(' '.join(subset))
